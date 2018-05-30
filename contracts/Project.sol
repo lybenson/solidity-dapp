ae710c292755a7e33565412cb59cb738e7795a3e
lybenson <naivebenson@gmail.com>
2018-05-30 20:20:21 +0800

    ProjectList manager all project

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -14,9 +14,60 @@ pragma solidity ^0.4.17;
 
 // 全局函数require: require 是 Solidity 提供的断言机制，如果传入的条件不满足交易就被回滚，具体到我们的众筹合约，如果各种业务规则检查不通过，接口调用会直接失败
 
+// solidity中的mapping
+// 所有的 key 和 value 都必须是完全相同的类型
+// mappings 里面并没有存储所有的 key，因此无法获取所有的 key 列表
+// mappings 里面的值无法被遍历，只能通过 key 逐个去取
+
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+    function mul(uint a, uint b) internal pure returns (uint) {
+        uint c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function div(uint a, uint b) internal pure returns (uint) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint a, uint b) internal pure returns (uint) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint a, uint b) internal pure returns (uint) {
+        uint c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+// 管理所有智能合约的合约
+contract ProjectList {
+    using SafeMath for uint;
+    address[] public projects;
+
+    function createProject(string _description, uint _minInvest, uint _maxInvest, uint _goal) public {
+        address newProject = new Project(_description, _minInvest, _maxInvest, _goal, msg.sender);
+        projects.push(newProject);
+    }
+
+    function getProjects() public view returns(address[]) {
+        return projects;
+    }
+}
+
 contract Project {
 
-    // using SafeMath for uint; // SafeMath 保证数学运算的安全
+    using SafeMath for uint; // SafeMath 保证数学运算的安全
 
     // 资金支出细节
     struct Payment {
@@ -24,7 +75,8 @@ contract Project {
         uint amount; // 支出金额
         address receiver; // 接收人
         bool completed; // 是否已完成
-        address[] voters; // 参与资金支出投票的投资人
+        mapping(address => bool) voters; // 参与资金支出投票的投资人
+        uint voterCount;
     }
 
     address public owner; // 项目所有者(address类型存储有效的以太坊地址, 账户余额 address.balance，发起转账 address.transfer 等)
@@ -32,16 +84,25 @@ contract Project {
     uint public minInvest; // 最小投资金额
     uint public maxInvest; // 最大投资金额
     uint public goal; // 融资上限
-    address[] public investors; // 投资人列表
+
+    uint public investorCount;
+    mapping(address => uint) public investors; // 投资人列表
+    // address[] public investors; 
     Payment[] public payments; // 资金支出列表
 
     // 智能合约构造函数, 传入所有合约的基本属性
-    constructor(string _description, uint _minInvest, uint _maxInvest, uint _goal) public {
-        owner = msg.sender;
+    constructor(string _description, uint _minInvest, uint _maxInvest, uint _goal, address _owner) public {
         description = _description;
         minInvest = _minInvest;
         maxInvest = _maxInvest;
         goal = _goal;
+        owner = _owner;
+    }
+
+    // modifier可以给其他函数添加其内部代码
+    modifier ownerOnly() {
+        require(msg.sender == owner);
+        _;
     }
 
     // 参与项目投资的接口, 投资人调用该接口时要求发送满足条件的资金(payable),并且要求没有达到募资上线
@@ -54,13 +115,13 @@ contract Project {
         newBalance = address(this).balance.add(msg.value);
         require(newBalance <= goal);
 
-        investors.push(msg.sender);
+        // investors.push(msg.sender);
+        investors[msg.sender] = msg.value;
+        investorCount += 1;
     }
 
     // 发起资金支出请求，要求传入资金支出的细节信息
-    function createPayment(string _description, uint _amount, address _receiver) public {
-
-        require(msg.sender == owner);
+    function createPayment(string _description, uint _amount, address _receiver) ownerOnly public {
 
         require(address(this).balance >= _amount); // 指定账户余额不大于要转出的金额
 
@@ -69,7 +130,7 @@ contract Project {
             amount: _amount,
             receiver: _receiver,
             completed: false,
-            voters: new address[](0)
+            voterCount: 0
         });
 
         payments.push(newPayment);
@@ -79,41 +140,32 @@ contract Project {
     function approvePayment(uint index) public {
         Payment storage payment = payments[index];
 
-        // must be investor to vote
-        bool isInvestor = false;
-        for (uint i = 0; i < investors.length; i++) {
-            isInvestor = investors[i] == msg.sender;
-            if (isInvestor) {
-                break;
-            }
-        }
-        require(isInvestor);
+        // 投票人存在于投资人列表中
+        require(investors[msg.sender] > 0);
 
-        // can not vote twice
-        bool hasVoted = false;
-        for (uint j = 0; j < payment.voters.length; j++) {
-            hasVoted = payment.voters[j] == msg.sender;
-            if (hasVoted) {
-                break;
-            }
-        }
-        require(!hasVoted);
+        // 投票人不存在于投票人列表中
+        require(!payment.voters[msg.sender]);
 
-        payment.voters.push(msg.sender);
+        // 将投票人加入投票人列表中
+        payment.voters[msg.sender] = true;
+        payment.voterCount += 1;
     }
 
     // 完成资金支出, 需要指定是哪笔支出，即调用该接口给资金接收方转账，不能重复转账，并且赞成票数超过投资人数量的 50%；
-    function doPayment(uint index) public {
-
-        require(msg.sender == owner);
+    function doPayment(uint index) ownerOnly public {
 
         Payment storage payment = payments[index];
 
-        require(address(this).balance >= payment.amount); // 指定账户余额不大于要转出的金额
-
+        // 支出交易未完成
         require(!payment.completed);
-        require(payment.voters.length > (investors.length / 2));
 
+        // 指定账户余额不少于要转出的金额
+        require(address(this).balance >= payment.amount); 
+
+        // 赞成票数超过投资人数量的 50%
+        require(payment.voterCount > (investorCount / 2));
+
+        // 转出到账户
         payment.receiver.transfer(payment.amount);
         payment.completed = true;
     }
