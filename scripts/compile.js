b4db76cdfdfc4f8bbea7a456692ca28b5191f4d4
lybenson <naivebenson@gmail.com>
2018-06-01 10:32:33 +0800

    unit test

--- scripts/compile.js
+++ scripts/compile.js
@@ -2,27 +2,44 @@ const fs = require('fs-extra')
 const path = require('path')
 const solc = require('solc')
 
-// cleanup
+// 1. cleanup
 const compiledDir = path.resolve(__dirname, '../compiled')
 fs.removeSync(compiledDir)
 fs.ensureDirSync(compiledDir)
 
 // compile
-const contractPath = path.resolve(__dirname, '../contracts', 'Car.sol')
-const contractSource = fs.readFileSync(contractPath, 'utf8')
+// const contractPath = path.resolve(__dirname, '../contracts', 'Car.sol')
+// const contractSource = fs.readFileSync(contractPath, 'utf8')
 
-const result = solc.compile(contractSource, 1)
-console.log(result)
+// const result = solc.compile(contractSource, 1)
+// console.log(result)
 
-// check errors
-if (Array.isArray(result.errors) && result.errors.length) {
-  throw new Error(result.errors[0])
-}
+// 2. search all contracts
+const contractFiles = fs.readdirSync(path.resolve(__dirname, '../contracts'));
 
-// save to disk
-Object.keys(result.contracts).forEach(name => {
-  const contractName = name.replace(/^:/, '')
-  const filePath = path.resolve(__dirname, '../compiled', `${contractName}.json`)
-  fs.outputJsonSync(filePath, result.contracts[name]);
-  console.log(`save compiled contract ${contractName} to ${filePath}`)
+console.log(contractFiles)
+
+contractFiles.forEach(contractFile => {
+  // 2.1 compile
+  const contractPath = path.resolve(__dirname, '../contracts', contractFile)
+  const contractSource = fs.readFileSync(contractPath, 'utf8')
+  const result = solc.compile(contractSource, 1)
+  console.log(`file compiled: ${contractFile}`)
+
+  // 2.2 check errors
+   if (Array.isArray(result.errors) && result.errors.length) {
+      throw new Error(result.errors[0]);
+   }
+
+  // 2.3 save to disk
+  // save to disk
+  Object.keys(result.contracts).forEach(name => {
+    const contractName = name.replace(/^:/, '')
+    const filePath = path.resolve(__dirname, '../compiled', `${contractName}.json`)
+    fs.outputJsonSync(filePath, result.contracts[name]);
+    console.log(`save compiled contract ${contractName} to ${filePath}`)
+  })
 })
+
+
+
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,7 @@ const HDWalletProvider = require('truffle-hdwallet-provider')
 
 
 // 1. 拿到 bytecode
-const contractPath = path.resolve(__dirname, '../compiled/Car.json')
+const contractPath = path.resolve(__dirname, '../compiled/ProjectList.json')
 const { interface, bytecode } = require(contractPath)
 
 // 2. 配置 provider
@@ -16,21 +16,29 @@ const provider = new HDWalletProvider(
 // 3. 初始化 web3 实例
 const web3 = new Web3(provider)
 
-function deploy() {
-
+(async () => {
   // 4. 获取钱包里面的账户
-  web3.eth.getAccounts().then(accounts => {
-    console.log('部署合约的账户：', accounts[0])
+  const accounts = await web3.eth.getAccounts();
+  console.log('合约部署账户:', accounts[0]);
 
-    // 5. 创建合约实例并且部署
-    console.time('contract-deploy');
-    return new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['AUDI'] })
-    .send({ from: accounts[0], gas: '1000000' })
-  }).then(result => {
-    console.timeEnd('contract-deploy');
-    console.log('合约部署成功：', result.options.address);
-  })
-}
+  // 5. 创建合约实例并且部署
+  console.time('合约部署耗时');
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({ data: bytecode })
+      .send({ from: accounts[0], gas: '5000000' });
+  console.timeEnd('合约部署耗时');
+
+  const contractAddress = result.options.address;
+
+  console.log('合约部署成功:', contractAddress);
+  console.log('合约查看地址:', `https://rinkeby.etherscan.io/address/${contractAddress}`);
+
+  // 6. 合约地址写入文件系统
+  const addressFile = path.resolve(__dirname, '../address.json');
+  fs.writeFileSync(addressFile, JSON.stringify(contractAddress));
+  console.log('地址写入成功:', addressFile);
+
+  process.exit();
+})()
 
 deploy()
